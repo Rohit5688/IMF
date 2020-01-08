c7e14118846c1d12a544504db840884a7284e627
Rohit <rohit.verse@gmail.com>
2020-01-08 18:52:38 +0530

    updated runner for azure

--- IMF/src/test/java/runner/Runner_AT.java
+++ IMF/src/test/java/runner/Runner_AT.java
@@ -6,9 +6,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(dryRun = false, features = "classpath:/", glue = { "classpath:/stepDefinitions", "runner" }, tags = {
-		"@tag" }, plugin = { "html:target/cucumber-report/Regression", "json:target/cucumber-report/Regression.json",
-				"rerun:rerun/failed_scenarios.txt" })
+@CucumberOptions(dryRun = false, features = "src/test/resource/Features", glue = { "classpath:/stepDefinitions",
+		"runner" }, tags = { "@tag" }, plugin = { "html:target/cucumber-report/Regression",
+				"json:target/cucumber-report/Regression.json", "rerun:rerun/failed_scenarios.txt" })
 
 public class Runner_AT {
 }
